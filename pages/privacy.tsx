375bf09a49cb503d0f32c2a254bdc564dcc0bfd0
Julien <jp.tollie@gmail.com>
2025-02-26 17:18:22 +0800

    coverage

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -75,8 +75,8 @@ export default function PrivacyPolicy() {
           <h2 className="text-xl font-semibold text-black">9. Contact Us</h2>
           <p className="mt-2 text-midGrey">
             If you have any questions about this Privacy Policy, please contact us at{" "}
-            <a href="mailto:hello@worldmobile.club" className="hover:underline">
-            hello@worldmobile.club
+            <a href="mailto:hello@worldmobiledeals.com" className="hover:underline">
+            hello@worldmobiledeals.com
             </a>.
           </p>
         </section>
